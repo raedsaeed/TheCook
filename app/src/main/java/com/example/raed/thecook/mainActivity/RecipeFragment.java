e2a025de0d74b824a71fe4bcfd885dc35cf9cd09
raedsaeed <raedsaeed46@hotmail.com>
2018-05-06 23:24:53 +0200

    Add Tablet landscape mode

---
+++ app/src/main/java/com/example/raed/thecook/mainActivity/RecipeFragment.java
@@ -0,0 +1,10 @@
+package com.example.raed.thecook.mainActivity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by raed on 5/5/18.
+ */
+
+public class RecipeFragment extends Fragment {
+}
